04484145d2bf0a5c27b0ea2b430da2a506603ded
Francisco Valdes <ecodisonante@gmail.com>
2025-09-05 20:25:56 -0400

    fix: request en graphQL

--- bff/src/main/java/com/veterinaria/bff/service/GraphQLServiceImpl.java
+++ bff/src/main/java/com/veterinaria/bff/service/GraphQLServiceImpl.java
@@ -1,11 +1,23 @@
 package com.veterinaria.bff.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.veterinaria.bff.dto.GraphQLRequest;
+import com.veterinaria.bff.dto.MascotaResponseDTO;
+import com.veterinaria.bff.util.Json;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import lombok.extern.log4j.Log4j2;
+
 @Service
+@Log4j2
 public class GraphQLServiceImpl implements GraphQLService {
 
     private final RestTemplate restTemplate;
@@ -19,7 +31,25 @@ public class GraphQLServiceImpl implements GraphQLService {
 
     @Override
     public Object executeQuery(GraphQLRequest graphQLRequest) {
-        String graphqlUrl = azureFunctionsUrl + "/api/graphql";
-        return restTemplate.postForObject(graphqlUrl, graphQLRequest, Object.class);
+        String url = azureFunctionsUrl + "/api/graphql";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        String jsonRequest;
+        try {
+            jsonRequest = Json.write(graphQLRequest);
+        } catch (JsonProcessingException e) {
+            log.error("Error al convertir el objeto a JSON", e);
+            return null;
+        }
+
+        HttpEntity<String> requestEntity = new HttpEntity<>(jsonRequest, headers);
+
+        ResponseEntity<Object> response = restTemplate.exchange(
+                url,
+                HttpMethod.PUT,
+                requestEntity,
+                Object.class);
+        return response.getBody();
     }
 }
